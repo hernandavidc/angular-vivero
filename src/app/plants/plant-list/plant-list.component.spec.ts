6452ec39709073a7e2ff96bfa22555f7a3e22408
Hernan Alvarez Caballero <hd.alvarez@uniandes.edu.co>
2023-11-05 15:58:43 -0500

    Update plant-list.component.spec.ts

--- src/app/plants/plant-list/plant-list.component.spec.ts
+++ src/app/plants/plant-list/plant-list.component.spec.ts
@@ -1,8 +1,6 @@
 /* tslint:disable:no-unused-variable */
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
-import { By } from '@angular/platform-browser';
-import { DebugElement } from '@angular/core';
 
 import { PlantListComponent } from './plant-list.component';
 import { of } from 'rxjs';
