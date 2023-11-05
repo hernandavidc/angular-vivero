79ffd96552a6ef3f7409a555e880c2cc7f84f2ad
Hernan Alvarez Caballero <hd.alvarez@uniandes.edu.co>
2023-11-05 15:34:08 -0500

    Done Punto 2 - Add totals

--- src/app/plants/plant-list/plant-list.component.ts
+++ src/app/plants/plant-list/plant-list.component.ts
@@ -9,13 +9,21 @@ import { Plant } from '../../models';
 })
 export class PlantListComponent implements OnInit {
   plants: Plant[] = [];
+  totalInterior: number | boolean = false;
+  totalExterior: number | boolean = false;
 
   constructor(private plantService: PlantService) { }
+
+  calculateTotals(plants: Plant[]): void {
+    this.totalInterior = plants.filter(plant => plant.tipo.toLowerCase() === 'interior').length;
+    this.totalExterior = plants.filter(plant => plant.tipo.toLowerCase()  === 'exterior').length;
+  }
 
   ngOnInit(): void {
     this.plantService.getPlants().subscribe({
       next: (data: Plant[]) => {
         this.plants = data;
+        this.calculateTotals(data);
       },
       error: (err) => {
         console.error('There was an error fetching the plants:', err);
